bbe84fede9c8b6d2d7429e26495ec0b5390df7ff
Jose Andres Trujillo <trujillohenaresj@gmail.com>
2023-04-21 17:45:47 +0200

    ultimos cambios

--- addons/sql_assistant/__manifest__.py
+++ addons/sql_assistant/__manifest__.py
@@ -1,15 +1,34 @@
+# -*- coding: utf-8 -*-
 {
-    'name': 'SQL Assistant',
-    'version': '1.0',
-    'summary': 'Module for SQL Assistance',
-    'description': 'Module for SQL Assistance',
-    'author': 'Your Name',
-    'depends': ['base', 'model_sql_assistant'],
+    'name': "sqlAssistant",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/sql_assistant_view.xml',
+        'views/views.xml',
     ],
-    'application': True,
-    'auto_install': False,
-    'installable': True,
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
 }
--- addons/sql_assistant/models/models.py
+++ addons/sql_assistant/models/models.py
@@ -1,13 +1,88 @@
+# -*- coding: utf-8 -*-
+
 from odoo import models, fields, api
+import psycopg2
+import psycopg2.extras
+import json
 
-class SqlAssistant(models.Model):
-    _name = 'model_sql_assistant'
-    _description = 'SQL Assistant'
+def get_tables(connection):
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
-    query = fields.Char()
-    result = fields.Text(readonly=True)
+    cursor.execute("""SELECT table_schema, table_name
+                      FROM information_schema.tables
+                      WHERE table_schema != 'pg_catalog'
+                      AND table_schema != 'information_schema'
+                      AND table_type='BASE TABLE'
+                      ORDER BY table_schema, table_name""")
 
-    @api.multi
+    tables = cursor.fetchall()
+    cursor.close()
+    return tables
+
+
+def format_tables_into_string(tables):
+    val = ""
+    for row in tables:
+        val += "{}.{}\n".format(row["table_schema"], row["table_name"])
+    return val
+
+
+data_types = {
+    "integer": "integer",
+    "character varying": "varchar",
+    "timestamp without time zone": "timestamp",
+    "bytea": "bytea",
+    "numeric": "numeric",
+    "double precision": "double precision",
+    "date": "date",
+    "text": "text",
+    "boolean": "boolean"
+}
+
+class sql_assistant(models.Model):
+    conn = psycopg2.connect(database="odoodb",
+                                user="odoo",
+                                password="odoo15@2021",
+                                host="db",
+                                port="5432")
+    
+
+    _name = 'sql_assistant.sql_assistant'
+    _description = 'sql_assistant.sql_assistant'
+
+
+    query = fields.Text(string='Consulta')
+    result = fields.Text(string='Resultado', readonly=True)
+
+    
+
     def execute_query(self):
-        self.ensure_one()
-        self.result = 'TODO: Implement query execution'
+        query = self.query
+        # self.result = format_tables_into_string(get_tables(self.conn))
+        cur = self.conn.cursor()
+
+        # Obtener el script de creación de la base de datos
+        cur.execute("""SELECT table_name
+                      FROM information_schema.tables
+                      WHERE table_schema != 'pg_catalog'
+                      AND table_schema != 'information_schema'
+                      AND table_type='BASE TABLE'
+                      ORDER BY table_name""")
+        script = ""
+        tables = cur.fetchall()
+        for table in tables:
+            table_name = table[0]
+            cur.execute(f"SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{table_name}';")
+            columns = cur.fetchall()
+            create_table = f"CREATE TABLE {table_name} (" 
+            for column in columns:
+                tipo =data_types.get(column[1], "NO ESTA")
+                create_table += f"{column[0]} {tipo},"
+            create_table = create_table[:-1] + ");"
+            
+            script += create_table + "\n\n\n"
+       
+       # Cerrar el cursor y la conexión
+        cur.close()
+        
+        self.result = script
