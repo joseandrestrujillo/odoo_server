8951cc592a6cfaee660ffb72cb5d6a446a67fff8
root <root@joseandres>
2023-04-25 15:47:52 +0200

    Paquete social

--- addons/sql_assistant/__manifest__.py
+++ addons/sql_assistant/__manifest__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "sqlAssistant",
-
+    'name': "Asistente SQL",
+    'application': True,
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Cualquier consulta a los datos de tu empresa, sin complicaciones.
+        """,
 
     'description': """
-        Long description of module's purpose
+        Este modulo te permite realizar consultas sobre los datos de tu e-comerce en lenguaje natural.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Ai2SQL",
+    'website': "http://www.google.es",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -26,6 +26,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
+        'views/assets.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- addons/sql_assistant/models/models.py
+++ addons/sql_assistant/models/models.py
@@ -13,79 +13,52 @@ traslator = SQL_Traslator(api_key=api_key)
 class sql_assistant_record(models.Model):
     _name = 'sql_assistant.record'
     _description = 'SQL Assistant Record'
-
     sql_assistant_id = fields.Many2one('sql_assistant.sql_assistant', ondelete='cascade')
-    message_main_attachment_id = fields.Char(string='message_main_attachment_id')
-    name = fields.Char(string='name')
-    currency_id = fields.Char(string='currency_id')
-    code = fields.Char(string='code')
-    deprecated = fields.Char(string='deprecated')
-    user_type_id = fields.Char(string='user_type_id')
-    internal_type = fields.Char(string='internal_type')
-    internal_group = fields.Char(string='internal_group')
-    reconcile = fields.Char(string='reconcile')
-    note = fields.Char(string='note')
-    company_id = fields.Char(string='company_id')
-    group_id = fields.Char(string='group_id')
-    root_id = fields.Char(string='root_id')
-    is_off_balance = fields.Char(string='is_off_balance')
-    create_uid = fields.Char(string='create_uid')
-    create_date = fields.Char(string='create_date')
-    write_uid = fields.Char(string='write_uid')
-    write_date = fields.Char(string='write_date')
+    data = fields.Serialized()
+    display_name = fields.Char(compute='_compute_display_name')
+    def _generate_field_label(self, field_name):
+        return field_name.replace('_', ' ').capitalize()
 
-class sql_assistant(models.Model):
-    _name = 'sql_assistant.sql_assistant'
-    _description = 'sql_assistant.sql_assistant'
+    @api.depends('data')
+    def _compute_display_name(self):
+        for record in self:
+            if record.data.get('headers', False):
+                record.display_name = '\n|\n'.join(f'{self._generate_field_label(k)}' for k, v in record.data.get('headers', {}).items()) if record.data else _('Undefined')
+            else:
+                record.display_name = '\n|\n'.join(f'{v}' for k, v in record.data.items()) if record.data else _('Undefined')
+            
+    class sql_assistant(models.Model):
+        _name = 'sql_assistant.sql_assistant'
+        _description = 'sql_assistant.sql_assistant'
 
+        prompt = fields.Text(string='Consulta')
+        record_ids = fields.One2many('sql_assistant.record', 'sql_assistant_id', string='Records')
 
-    prompt = fields.Text(string='Consulta')
-    record_ids = fields.One2many('sql_assistant.record', 'sql_assistant_id', string='Records')
+        def datetime_to_string(self, data):
+            for item in data:
+                for key, value in item.items():
+                    if isinstance(value, datetime):
+                        item[key] = value.strftime('%Y-%m-%d %H:%M:%S')
+            return data
 
-    def datetime_to_string(self, data):
-        for item in data:
-            for key, value in item.items():
-                if isinstance(value, datetime):
-                    item[key] = value.strftime('%Y-%m-%d %H:%M:%S')
-        return data
-
-    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False,submenu=False):
-        result = super(self._name, self).fields_view_get(cr, uid, view_id, view_type, context, toolbar,submenu)
-
-    def execute_query(self):
-        #sql_query = traslator.convert_to_sql(text_for_query=self.prompt)
-        sql_query = self.prompt
-        self.env.cr.execute(sql_query)
-        records = self.env.cr.dictfetchall()
-        self.record_ids.unlink()
-        records = self.datetime_to_string(records)
-        # Crear vista de árbol dinámicamente
-        tree_view = self.env['ir.ui.view'].create({
-            'name': 'sql_assistant.record.tree',
-            'type': 'tree',
-            'model': 'sql_assistant.record',
-            'arch': self._generate_tree_view_arch(records),
-        })
-
-        # Actualizar la relación entre la vista y el campo 'record_ids'
-        self.write({
-            'record_ids': [(6, 0, [r.id for r in tree_view])]
-        })
-
-        # Crear registros
-        for record in records:
-            self.env['sql_assistant.record'].create({
-                'sql_assistant_id': self.id,
-                **record
-            })
-
-    def _generate_tree_view_arch(self, records):
-        if not records:
-            return '<tree><field name="display_name"/></tree>'
-
-        columns = records[0].keys()
-        tree_view_arch = '<tree>'
-        for col in columns:
-            tree_view_arch += f'<field name="{col}"/>'
-        tree_view_arch += '</tree>'
-        return tree_view_arch
+        def execute_query(self):
+            sql_query = traslator.convert_to_sql(text_for_query=self.prompt)
+            self.env.cr.execute(sql_query)
+            records = self.env.cr.dictfetchall()
+            self.record_ids.unlink()
+            records = self.datetime_to_string(records)
+            first = True
+            for record in records:
+                if first:
+                    self.env['sql_assistant.record'].create({
+                        'sql_assistant_id': self.id,
+                        'data': {
+                            'is_headers': True,
+                            'headers': record
+                        }
+                    })
+                    first = False
+                self.env['sql_assistant.record'].create({
+                    'sql_assistant_id': self.id,
+                    'data': record
+                })
